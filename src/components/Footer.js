4bc1448d382b2c6180456c20361c3d195071fba2
Anurag Sidhu <anuragsidhu72@gmail.com>
2024-06-08 16:29:02 +0530

    Navbar and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { FaPhone } from "react-icons/fa6";
+import { CiMail } from "react-icons/ci";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FiYoutube } from "react-icons/fi";
 
-const Footer = () => {
+function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <div className=' p-5 flex text-white bg-[#007569]  items-center justify-center'>
+        <div className='flex flex-col md:flex-row  space-x-10 '>
+          <div className='  flex items-center justify-center md:justify-start md:items-start text-3xl font-bold '>
+            Doctor +
+          </div>
+          <div className='w-fit  '>
+            Thank you for visiting my website, and  I look forward
+            to being a part of <br /> your wellness journey. Let's thrive
+            together!</div>
+          <div>
+            <div className=' w-fit flex items-center space-x-3'><FaPhone />
+              <p>
+                +91-1234567890
+              </p>
+            </div>
+            <div className=' w-fit flex items-center space-x-3'> <CiMail />
+              <p>
+                contact@gmail.com
+              </p>
+            </div>
+            <div className=' w-fit flex space-x-5'><FaFacebook /><FaInstagram /><FiYoutube /></div>
+          </div>
+        </div>
+      </div>
+      <div className='bg-[#276A7B] text-sm text-white flex items-center justify-evenly font-light p-1'>
+        <div className='border-t-2 flex flex-col md:flex-row space-y-3 md:space-y-0 items-center justify-evenly w-3/5'>
+          <div>Copyright 2023 . All right Reserved</div>
+          <div className='flex space-x-8'>
+            <p>Privacy Policy</p>
+            <p>Term Of Service</p>
+            <p>Legal Info</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { CiSearch } from "react-icons/ci";
+import { IoIosArrowDown } from "react-icons/io";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import { Link } from 'react-router-dom';
+import { FaUser } from "react-icons/fa";
 
-const Navbar = () => {
+function NavBar() {
+  const [open, setOpen] = useState(false);
+  const getdata = localStorage.getItem("token");
+
   return (
-    <div>Navbar</div>
-  )
+    <div className={`p-4 ${open ? 'bg-white' : 'bg-transparent'}`}>
+      <div className={`rounded-full flex flex-col md:flex-row items-center justify-between p-4 ${open ? 'bg-white text-black' : 'bg-[#007569] text-white'}`}>
+
+
+      <div className={`flex space-x-8 ${open ? "bg-[#007569] w-full p-2 rounded-lg text-white" :""}`}>
+        <div className='font-bold lg:text-2xl'>
+          <Link to="/" className={open ? 'text-white ' : 'text-white '}>Doctor +</Link>
+        </div>
+
+        <div className='lg:hidden' onClick={() => setOpen(!open)}>
+          {open ? <RxCross1 className='text-2xl' /> : <RxHamburgerMenu className='text-2xl' />}
+        </div>
+      </div>
+
+        <div className={`lg:flex space-x-8   ${open ? 'flex ' : 'hidden'} flex-col lg:flex-row items-center w-full lg:w-auto`}>
+          <div className={`flex items-center space-x-1 rounded-lg w-full lg:w-96 p-2 mt-4 lg:mt-0 ${open ? 'bg-[#007569] text-white' : 'bg-white'}`}>
+            <CiSearch className={open ? 'text-white' : 'text-black'} />
+            <input placeholder='Search Doctor' className={`focus:outline-none w-96 ${open ? 'text-white placeholder-white' : 'text-black placeholder-black'}`} />
+          </div>
+
+          <div className='list-none  w-full   flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-3 mt-4 lg:mt-0'>
+            <Link to="/" className=' '><li className={`cursor-pointer  flex items-center justify-center ${open ? 'bg-[#007569]  text-white p-2 rounded-md' : ''}`}>Home</li></Link>
+            <Link to="/"><li className={`cursor-pointer  flex items-center justify-center ${open ? 'bg-[#007569]  text-white p-2 rounded-md' : ''}`}>Service</li></Link>
+            <Link to="/doctors"><li className={`cursor-pointer  flex items-center justify-center ${open ? 'bg-[#007569]  text-white p-2 rounded-md' : ''}`}>Doctors</li></Link>
+            <Link to="/about"><li className={`cursor-pointer  flex items-center justify-center ${open ? 'bg-[#007569] text-white p-2 rounded-md' : ''}`}>About Us <IoIosArrowDown className='text-xl' /></li></Link>
+          </div>
+
+          {getdata ? (
+            <Link to="/profile" className={`mt-4 lg:mt-0 ${open ? 'bg-[#007569] w-full flex items-center justify-center text-white p-2 rounded-md' : ''}`}>
+              <FaUser className='text-2xl cursor-pointer' />
+            </Link>
+          ) : (
+            <Link to="/login" className='mt-4 lg:mt-0'>
+              <div className={` px-1 py-2 w-32 rounded-lg cursor-pointer ${open ? 'bg-[#007569] text-white' : 'bg-white text-black'}`}>
+                Login / Signup
+              </div>
+            </Link>
+          )}
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default NavBar;
