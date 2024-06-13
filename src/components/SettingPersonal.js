265b6fc51cae4e82972fc018b9332650769ac6b8
Ayesha tazeen <ayesha.khann9864@gmail.com>
2024-06-13 15:30:28 +0530

    profile done

--- src/components/SettingPersonal.js
+++ src/components/SettingPersonal.js
@@ -1,43 +1,149 @@
+
+
+
 import React from 'react'
-import { IoIosArrowForward } from "react-icons/io";
+import { useState } from 'react';
+
+
+
 const SettingPersonal = () => {
+
+
+
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [Phoneno, setPhoneno] = useState("");
+  const [gender, setGender] = useState("");
+  const [bio, setBio] = useState("");
+  const [language, setLanguage] = useState("")
+  // const [profilepic, setProfilepic] = useState("")
+
+
+
+  const handlePersonalInfoSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await fetch(
+        "https://doctors-backend-ztcl.onrender.com/updatesettings/660be57c3b9e529a2236f462",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ gender: gender, language: language, bio: bio, Phoneno: Phoneno, firstname: firstname, lastname: lastname }),
+        }
+      );
+
+      if (data.ok) {
+        alert("added successfully")
+      }
+      else {
+        alert("Try Again and reload the page")
+      }
+
+    } catch (error) {
+      alert("Try Again and reload the page")
+    }
+  }
+
+
+
+  // const handleProfilePicChange = (e) => {
+  //   const file = e.target.files[0]; // Get the first file from the input
+  //   setProfilepic(file); // Set the file as the profile picture
+  // }
+
   return (
     <div className='bg-white p-4 w-full space-y-2 rounded-lg shadow-lg'>
-      <div className='flex items-center justify-between'>
-        <p>Personal Details</p>
-        <div className='flex items-center justify-center'>
-        <p>Dashboard</p>    
-        <p><IoIosArrowForward/></p>    
-        <p>Setting</p>    
-        <p><IoIosArrowForward/></p>    
-        <p>Personal Details</p>    
+      <div className='flex items-center flex-col md:flex-row justify-between'>
+        <h1 className='text-[24px] md:text-[28px] font-semibold'>Personal Details</h1>
+
+        <div className='text-sm font-semibold text-center md:text-right mt-2 md:mt-0'>
+          Dashboard {">"} Security <span className='text-[#007569]'>{'>'}Personal Details </span>
         </div>
       </div>
-       
-       <div className=''>
-      <div className='bg-[#F7F7F7] flex  flex-col   p-3 space-y-3 '>
-        <p className='w-fit'>Your Details</p>
-        <input placeholder='Enter Gender' className='p-2 focus:outline-none rounded-xl'/>
-        <input placeholder='Enter User Name' className='p-2 focus:outline-none rounded-xl'/>
-        <input placeholder='Enter Last Name' className='p-2 focus:outline-none rounded-xl'/>
-        <input placeholder='Personal Mobile Number' className='p-2 focus:outline-none rounded-xl'/>
-        <textarea rows="4" cols="50" placeholder='About Me' className='p-2 focus:outline-none rounded-xl'></textarea>
 
-        <div className='w-fit'>
-            <p className='w-fit'>Select Language</p>
-            <input placeholder='Enter Language' className='p-2 focus:outline-none rounded-xl'/>
-        </div>
+      <div className=''>
+        <div className='bg-[#F7F7F7] flex  flex-col   p-3 space-y-3 '>
+          <p className='w-fit'>Your Details</p>
+ <select
+            placeholder='Enter Gender'
+            className='p-2 focus:outline-none rounded-xl text-gray-500'
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <option value=''>Gender</option>
 
-        <div className='w-fit'>
-            <p className='w-fit'>Profile Photo</p>
-            <input type='file' />
+            <option value='Male'>Male</option>
+            <option value='Female'>Female</option>
+            <option value='other'>Other</option>
+          </select>
+
+          <input type='text'
+            placeholder='First name'
+            name='name'
+            value={firstname} className='p-2 focus:outline-none rounded-xl'
+            onChange={(e) => setFirstname(e.target.value)} />
+          <input placeholder='Last Name'
+            type='text'
+            name='name'
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)} className='p-2 focus:outline-none rounded-xl' />
+
+          <input placeholder='Personal Mobile Number' value={Phoneno}
+            onChange={(e) => setPhoneno(e.target.value)}
+            className='p-2 focus:outline-none rounded-xl' />
+
+
+          <textarea rows="4" cols="50" value={bio} onChange={(e) => setBio(e.target.value)} placeholder='About Me' className='p-2 focus:outline-none rounded-xl'></textarea>
+
+         
+          <select
+            placeholder='Enter Language'
+            className='p-2 focus:outline-none rounded-xl text-gray-500'
+            onChange={(e) => setLanguage(e.target.value)} 
+          >
+            <option value=''>select language</option>
+
+            <option value='English'>English</option>
+            <option value='hindi'>hindi</option>
+            <option value='spanish'>spanish</option>
+            <option value='other'>other</option>
+          </select>
+           
+
+
+
+          <div className='w-full'>
+
+            <label className='block pb-2 font-semibold'>Profile Picture</label>
+            <div className='border-[2px]  py-[6px] rounded-md border-dashed border-[#949494]'>
+              <input
+                placeholder='Drop file here to upload'
+                type='file'
+                name='photo'
+                // onChange={handleProfilePicChange}
+                className='hidden'
+                id='fileInput'
+              />
+              <div className='flex flex-row'>
+                <label
+                  htmlFor='fileInput'
+                  className='border-[2px] rounded-md px-2 py-0 text-xs w-fit p-1 text-white bg-[#276A7B] hover:bg-[#25515c] cursor-pointer flex justify-center items-center'
+                >
+                  Select File
+                </label>
+                <p className='text-[#949494] pl-1 '>Drop files here to upload</p>
+              </div>
+            </div>
+          </div>
+
+          <p className=' flex justify-end mt-3'>
+            <button className='bg-[#007569] px-3 py-1 rounded-lg text-white'
+              onClick={handlePersonalInfoSubmit}>
+              Save Changes
+            </button>
+          </p>
         </div>
-      <p className=' flex justify-end mt-3'>
-        <button className='bg-[#007569] px-3 py-1 rounded-lg text-white'>
-        Save Changes
-        </button>
-        </p>
-       </div>
       </div>
 
     </div>
@@ -45,3 +151,4 @@ const SettingPersonal = () => {
 }
 
 export default SettingPersonal
+
